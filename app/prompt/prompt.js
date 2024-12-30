6d61d50210d198186da713683695cbebe17a14ba
Jayson <xantaren@gmail.com>
2024-12-30 22:55:40 +0800

    Provide system prompt with current time

--- app/prompt/prompt.js
+++ app/prompt/prompt.js
@@ -12,8 +12,10 @@ class Prompt {
   messages = [];
 
   constructor() {
+    const TIME_PROMPT = `Current time:[[${new Date().toString()}]]. Always report the offset time accounting for user timezone`
     this
-      .write(ROLE_SYSTEM, config.APP_INIT_PROMPT || t('__COMPLETION_DEFAULT_SYSTEM_PROMPT'))
+      .write(ROLE_SYSTEM,
+            `${TIME_PROMPT}\n${config.APP_INIT_PROMPT || t('__COMPLETION_DEFAULT_SYSTEM_PROMPT')}`)
       .write(ROLE_HUMAN, `${t('__COMPLETION_DEFAULT_HUMAN_PROMPT')(config.HUMAN_NAME)}${config.HUMAN_INIT_PROMPT}`)
       .write(ROLE_AI, `${t('__COMPLETION_DEFAULT_AI_PROMPT')(config.BOT_NAME)}${config.BOT_INIT_PROMPT}`);
   }
@@ -30,6 +32,11 @@ class Prompt {
     return encoded.length;
   }
 
+  replaceWithCurrentDate(inputString) {
+    const currentDate = new Date().toString();
+    return inputString.replace(/\[\[.*?\]\]/, `[[${currentDate}]]`);
+  }
+
   erase() {
     if (this.messages.length > 0) {
       this.messages.pop();
@@ -42,8 +49,18 @@ class Prompt {
    * @param {string} content
    */
   write(role, content = '') {
+    if (config.APP_DEBUG) {
+      let msg = `Message length: [${this.messages.length}]. Token count: [${this.tokenCount}].`;
+      console.info(msg);
+    }
     if (this.messages.length >= MAX_MESSAGES || this.tokenCount >= MAX_TOKENS) {
       this.messages.splice(3, 1);
+    }
+    for (const message of this.messages) {
+      if (message.role === ROLE_SYSTEM) {
+        message.content = this.replaceWithCurrentDate(message.content);
+        break; // Assuming there would only be one system prompt
+      }
     }
     this.messages.push(new Message({ role, content: addMark(content) }));
     return this;
