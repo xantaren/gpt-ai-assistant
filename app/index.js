135b26c450bec116fa5e9e149d8716ed3759d30c
Jayson Liang <jayson.liang@seidor.com>
2025-01-02 16:59:38 +0800

    Refactored and simplified getting and setting prompt
    Removed residuals
    
    Fix variable names
    
    poi

--- app/index.js
+++ app/index.js
@@ -1,14 +1,13 @@
 import handleEvents from './app.js';
 import { printHistories } from './history/index.js';
 import {
-  getPrompt, printPrompts, removePrompt, setPrompt,
+  getPrompt, removePrompt, setPrompt,
 } from './prompt/index.js';
 
 export {
   handleEvents,
   printHistories,
   getPrompt,
-  printPrompts,
   removePrompt,
   setPrompt,
 };
--- app/prompt/index.js
+++ app/prompt/index.js
@@ -10,28 +10,25 @@ const prompts = new Map();
  * @returns {Prompt}
  */
 const getPrompt = (userId) => {
-  if (prompts.get(userId)) {
-    if (config.APP_DEBUG) {
-      console.info(`Retrieved prompt for [${userId}] from memory, length: [${prompts.get(userId).messages?.length}]`);
-    }
-    return prompts.get(userId);
-  }
-  // Set to empty prompt to prevent duplicating messages created on object initialization
-  prompts.set(userId, new Prompt(true));
   const retrievedPromptsObj = getPrompts();
   if (config.APP_DEBUG) console.info(`retrievedPromptsObj: ${JSON.stringify(retrievedPromptsObj)}`);
-  const prompt = retrievedPromptsObj[userId];
-  if (prompt) {
-    if (config.APP_DEBUG) console.info(`Set prompt from storage to memory for [${userId}], length: [${prompt.messages?.length}]`);
-    prompt.messages.forEach((message) => {
+  const storedPrompt = retrievedPromptsObj[userId];
+  if (storedPrompt) {
+    // Initialize a completely empty prompt to prevent stored and default prompts from overlapping or duplicating each other
+    const newPrompt = new Prompt(true);
+    let messageLength = storedPrompt.messages?.length;
+    if (config.APP_DEBUG) console.info(`Set prompt from storage to memory for [${userId}], length: [${messageLength}]`);
+    let count = 1;
+    storedPrompt.messages.forEach((message) => {
       const newMessage = new Message(message);
-      prompts.get(userId).messages.push(newMessage);
+      newPrompt.messages.push(newMessage);
+      if (config.APP_DEBUG) console.info(`${count} of ${messageLength}: [${newMessage.role}] ${newMessage.content}`);
+      count++;
     })
-  } else {
-    // No existing prompt, create prompt with default messages
-    prompts.set(userId, new Prompt());
+    return newPrompt;
   }
-  return prompts.get(userId);
+  // A new prompt with default system, human and assistant message
+  return new Prompt();
 }
 
 /**
@@ -43,7 +40,6 @@ const setPrompt = (userId, prompt) => {
     const newPrompt = {};
     newPrompt[userId] = prompt;
     await setPrompts(newPrompt).then(() => {
-      prompts.set(userId, prompt);
       if (config.APP_DEBUG) console.info(`Successfully set prompt for [${userId}], length: [${prompt.messages?.length}]`);
     });
   })()
@@ -57,16 +53,9 @@ const removePrompt = (userId) => {
     const newPrompt = {};
     newPrompt[userId] = new Prompt();
     await setPrompts(newPrompt).then(() => {
-      prompts.delete(userId);
-      if (config.APP_DEBUG) console.info(`Successfully deleted prompt for [${userId}]`);
+      if (config.APP_DEBUG) console.info(`Successfully reset prompt for [${userId}]`);
     });
   })()
-};
-
-const printPrompts = () => {
-  if (Array.from(prompts.keys()).length < 1) return;
-  const content = Array.from(prompts.keys()).map((userId) => `\n=== ${userId.slice(0, 6)} ===\n${getPrompt(userId)}\n`).join('');
-  console.info(content);
 };
 
 export {
@@ -74,7 +63,6 @@ export {
   getPrompt,
   setPrompt,
   removePrompt,
-  printPrompts,
 };
 
 export default prompts;
