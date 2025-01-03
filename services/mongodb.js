b3a78a4818e420a5ae25a6745fad66f5bcbd9692
Jayson Liang <jayson.liang@seidor.com>
2025-01-03 14:49:40 +0800

    Integrate MongoDB for persistent storage

--- services/mongodb.js
+++ services/mongodb.js
@@ -4,23 +4,28 @@ import config from '../config/index.js';
 const username = config.MONGODB_USERNAME;
 const password = config.MONGODB_PASSWORD;
 const clusterUrl = config.MONGODB_CLUSTER_URL;
-const cluster = config.MONGODB_CLUSTER_NAME;
+const clusterName = config.MONGODB_CLUSTER_NAME;
 const dbName = config.MONGODB_DB_NAME;
 const collectionName = config.MONGODB_COLLECTION_NAME;
 
-const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/?retryWrites=true&w=majority&appName=${cluster}`;
+const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/?retryWrites=true&w=majority&appName=${clusterName}`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
+let client;
+let collection;
 
-await client.connect();
-
-const database = client.db(dbName);
-const collection = database.collection(collectionName);
-
-// An extremely simple KV store
+export async function initializeMongoDb() {
+    if (!client) {
+        if (config.APP_DEBUG) console.info(`Making connection to: ${uri}`);
+        client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db(dbName);
+        collection = database.collection(collectionName);
+        if (config.APP_DEBUG) console.info(`Initialized Mongo DB`);
+    }
+}
 
 export async function setKeyValue(key, value) {
+    if (config.APP_DEBUG) console.info(`Setting Key: [${key}], Value: [${value}]`);
     return await collection.updateOne(
         {key},
         {$set: {key, value}},
@@ -29,6 +34,7 @@ export async function setKeyValue(key, value) {
 }
 
 export async function getValueByKey(key) {
+    if (config.APP_DEBUG) console.info(`Getting value for: [${key}]`);
     const result = await collection.findOne({ key });
     return result ? result.value : null;
 }
--- storage/index.js
+++ storage/index.js
@@ -1,6 +1,7 @@
 import config from '../config/index.js';
-import { createEnvironment, ENV_TYPE_PLAIN, updateEnvironment } from '../services/vercel.js';
-import { fetchEnvironment } from '../utils/index.js';
+import {createEnvironment, ENV_TYPE_PLAIN, updateEnvironment} from '../services/vercel.js';
+import {fetchEnvironment} from '../utils/index.js';
+import {getValueByKey, initializeMongoDb, setKeyValue} from '../services/mongodb.js';
 
 const ENV_KEY = 'APP_STORAGE';
 
@@ -10,6 +11,34 @@ class Storage {
   data = {};
 
   async initialize() {
+    if (config.ENABLE_MONGO_DB) {
+      await this.initializeMongoDb();
+    } else {
+      await this.initializeVercel();
+    }
+  }
+
+  async initializeMongoDb() {
+    try {
+      await initializeMongoDb();
+
+      const value = await getValueByKey(ENV_KEY);
+
+      if (value === null) {
+        const initialData = JSON.stringify(this.data);
+        await setKeyValue(ENV_KEY, initialData);
+        if (config.APP_DEBUG) console.info(`No values found, initializing with current data`);
+        this.data = JSON.parse(initialData);
+      } else {
+        this.data = JSON.parse(value);
+        if (config.APP_DEBUG) console.info(`Data initialized: [${JSON.stringify(this.data)}]`);
+      }
+    } catch (error) {
+      console.error(`Failed to initialize MongoDB: ${error.message}`);
+    }
+  }
+
+  async initializeVercel() {
     if (!config.VERCEL_ACCESS_TOKEN) return;
     this.env = await fetchEnvironment(ENV_KEY);
     if (!this.env) {
@@ -36,22 +65,28 @@ class Storage {
    * @param {string} value
    */
   async setItem(key, value) {
+    this.data[key] = value;
     try {
-      this.data[key] = value;
-      if (!config.VERCEL_ACCESS_TOKEN) return;
-      await updateEnvironment({
-        id: this.env.id,
-        value: JSON.stringify(this.data, null, config.VERCEL_ENV ? 0 : 2),
-        type: ENV_TYPE_PLAIN,
-      });
+      if (config.ENABLE_MONGO_DB) {
+        await setKeyValue(ENV_KEY, JSON.stringify(this.data, null, config.VERCEL_ENV ? 0 : 2));
+      } else {
+        await this.setItemWithVercel(key, value);
+      }
 
     } catch (e) {
       if (config.APP_DEBUG) {
-        console.info(e.stack);
-        console.info(e.name);
         console.info(e.message);
       }
     }
+  }
+
+  async setItemWithVercel(key, value) {
+    if (!config.VERCEL_ACCESS_TOKEN) return;
+    await updateEnvironment({
+      id: this.env.id,
+      value: JSON.stringify(this.data, null, config.VERCEL_ENV ? 0 : 2),
+      type: ENV_TYPE_PLAIN,
+    });
   }
 }
 
