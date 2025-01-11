58fd790d5c31eee78d82968b99a07eb76b520eb7
Jayson <xantaren@gmail.com>
2025-01-11 11:27:03 +0800

    Disable addMark

--- utils/add-mark.js
+++ utils/add-mark.js
@@ -1,20 +1,8 @@
 import config from '../config/index.js';
 
+// TODO check if this can be removed
 const addMark = (text) => {
-  if (!text) return text;
-  const marks = ['？', '。', '！', '?', '.', '!'];
-  if (marks.some((mark) => text.endsWith(mark))) {
-    return text;
-  }
-  switch (config.APP_LANG) {
-    case 'zh':
-    case 'zh_TW':
-    case 'zh_CN':
-    case 'ja':
-      return `${text}。`;
-    default:
-      return `${text}.`;
-  }
+  return text;
 };
 
 export default addMark;
