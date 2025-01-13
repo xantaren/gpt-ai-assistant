4aba90c74c58f1eb3eea6d1b56271adadfe3a64d
Jayson Liang <xantaren@yahoo.com>
2025-01-13 10:11:57 +0800

    Experimental safety settings threshold value

--- services/gemini.js
+++ services/gemini.js
@@ -7,19 +7,19 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const safetySettings = [
     {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: "OFF",
     },
     {
         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: "OFF",
     },
     {
         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: "OFF",
     },
     {
         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: "OFF",
     },
 ];
 
